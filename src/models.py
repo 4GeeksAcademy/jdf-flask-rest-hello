90ebef4f0c2524af68038e65e59929d8aa3cb0ab
Juandomz91 <juandomz87@gmail.com>
2025-05-30 18:37:22 +0000

    meu

--- src/models.py
+++ src/models.py
@@ -6,14 +6,39 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(90))
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+class Follower(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_from_id: Mapped[int] = mapped_column(ForeignKey('user.id'), primary_key=True)
+    user_to_id: Mapped[int] = mapped_column(ForeignKey('user.id'), primary_key=True)
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+
+
+
+class Media(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[str] = mapped_column(Enum("image", "video", "carousel", name="media_type_enum"), nullable=False)
+    url: Mapped[str] = mapped_column(String(120))
+    post_id: Mapped[int] = mapped_column(ForeignKey('post.id'))
+
+class Comment(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(String(500))
+    author_id: Mapped[int] = mapped_column
+    post_id: Mapped[int] = mapped_column
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "is_active": self.is_active
             # do not serialize the password, its a security breach
         }
